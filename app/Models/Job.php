749d793b8e380aedaef1eb28f9f79f08994d1b53
HurasAdam <huradam@wp.pl>
2024-08-06 19:47:45 +0200

    Feat: Added Post, Job and Comment Models. Added Job migrations and Facotry.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,34 +1,12 @@
 <?php
 namespace App\Models;
-use Illuminate\Support\Arr;
-class Job{
 
-        public static function all():array{
-    return [
-        [
-            'id'=>1,
-            'title'=>'Director',
-            'salary'=>'$50.000'
-        ],
-        [
-            'id'=>2,
-            'title'=>'Programmer',
-            'salary'=>'$20.000'
-        ],
-        [
-            'id'=>3,
-            'title'=>'Teacher',
-            'salary'=>'$10.000'
-        ],
-    ];
-        }
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class Job extends Model{
 
-        public static function find(int $id):array{
- $job = Arr::first(static::all(), fn($job)=>$job['id'] ==$id);
- if(!$job){
-     abort(404);
- }
- return $job;
-        }
+    use HasFactory;
+protected $table = 'job_listings';
+protected $fillable = ['title','salary'];
 
 };
--- database/migrations/2024_08_05_190814_create_job_listings_table.php
+++ database/migrations/2024_08_05_190814_create_job_listings_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->foreignIdFor(App\Models\Employer::class);
             $table->string('salary');
             $table->timestamps();
         });
