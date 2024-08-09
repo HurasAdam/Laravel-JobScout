6e033878db4a62624d99dda4c73bada547db69bc
HurasAdam <huradam@wp.pl>
2024-08-09 21:59:04 +0200

    Feat: Added debug bar, disabled lazy loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,15 +2,15 @@
     <x-slot:heading>
         Jobs List
     </x-slot:heading>
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}">
-                    <b>{{ $job['title'] }}</b>:Pays {{ $job['salary'] }}per year
-                </a>
-
-            </li>
+            <div class="text-blue-500 font-bold text-sm">
+                {{ $job->employer->name }}
+            </div>
+            <a href="/jobs/{{ $job['id'] }}" class="block px-5 py-6 border border-gray-200 rounded-lg">
+                <b>{{ $job['title'] }}</b>:Pays {{ $job['salary'] }}per year
+            </a>
         @endforeach
-    </ul>
+    </div>
 
 </x-layout>
