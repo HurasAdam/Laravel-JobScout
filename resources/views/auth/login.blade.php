82d480298f57de5a304ebb4bb5797f5710e9f5a3
HurasAdam <huradam@wp.pl>
2024-08-11 15:23:30 +0200

    Feat: Added Register and Login routes, Added SessionController and controller methods, Added login resource view.

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,63 @@
+<x-layout>
+    <x-slot:heading>
+ Log In
+    </x-slot:heading>
+
+    <form method="POST" action="/login">
+        @csrf
+
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+             
+
+                <div class=" grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+<x-form-field>
+            <x-form-label label="Email" for="email"/>
+                    
+            <x-form-input
+                    type='email'
+                    name='email'
+                    id='email'
+                
+                    placeholder="johnDoe@gmail.com"
+                    />
+
+            <x-form-error name="email"/>
+</x-form-field>
+<x-form-field>
+            <x-form-label label="Password" for="password"/>
+                    
+            <x-form-input
+                    type='password'
+                    name='password'
+                    id='password'
+                
+                 
+                    />
+
+            <x-form-error name="password"/>
+</x-form-field>
+
+
+              
+                </div>
+
+{{--                <div class="mt-10">--}}
+{{--                    @if($errors->any())--}}
+{{--                        <ul>--}}
+{{--                            @foreach($errors->all() as $error)--}}
+{{--                                <li class="text-red-500 italic">{{ $error }}</li>--}}
+{{--                            @endforeach--}}
+{{--                        </ul>--}}
+{{--                    @endif--}}
+{{--                </div>--}}
+            </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+          <x-button href="/">Cancel</x-button>
+<x-form-button label="Register"/>
+        </div>
+    </form>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,3 +18,8 @@ Route::controller(JobController::class)->group(function(){
     Route::view("/contact","contact");
 });
 
+Route::get('/register',[RegisterUserController::class,'create']);
+Route::post('/register',[RegisterUserController::class,'store']);
+
+Route::get('/login',[SessionController::class,'create']);
+Route::post('/login',[SessionController::class,'store']);
