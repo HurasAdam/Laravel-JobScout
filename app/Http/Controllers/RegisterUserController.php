4201e816c10e79a5bc6785c95e25e4daec07cfd9
HurasAdam <huradam@wp.pl>
2024-08-12 18:31:28 +0200

    Feat: Added store method to RegisterUserController to handle account register.

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterUserController extends Controller
 {
@@ -11,7 +14,17 @@ class RegisterUserController extends Controller
    return view('auth.register');
     }
 
-    public function store(){
-  dd("Account has been created");
+    public function store(Request $request){
+ $validatedAttributes = $request->validate([
+
+  'first_name'=> ['required','max:50'],
+  'last_name'=> ['required','max:50'],
+  'email'=> ['required','max:254','email'],
+  'password'=> ['required',Password::min(6),'confirmed'],
+ ]);
+ $validatedAttributes['isAdmin'] = false;
+$user = User::create($validatedAttributes);
+Auth::login($user);
+return redirect('/');
     }
-}
+};
