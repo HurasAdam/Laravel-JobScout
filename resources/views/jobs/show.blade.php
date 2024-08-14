362d82fae533e8d45e2922e2b8c213e63adcc32f
HurasAdam <huradam@wp.pl>
2024-08-14 23:48:26 +0200

    Feat: Added auth middleware to secure routes.

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,11 +3,13 @@
         <h1>Job</h1>
     </x-slot:heading>
 
-    <h2 class="font-bold text-lg">{{$job['title']}}</h2>
+    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
     <span>This job pays:{{ $job->salary }}</span>
-    <p class="mt-6">
-<x-button href="/jobs/{{$job->id}}/edit" class="">
-    Edit Job 
-</x-button>
-    </p>
+    @can('edit-job', $job)
+        <p class="mt-6">
+            <x-button href="/jobs/{{ $job->id }}/edit" class="">
+                Edit Job
+            </x-button>
+        </p>
+    @endcan
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,24 @@ use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::controller(JobController::class)->group(function(){
+
     Route::view("/","home");
-    Route::get('/jobs', 'index');
-    Route::get('jobs/create',  'create');
-    Route::get('jobs/{job}', 'show');
-    Route::post("jobs",  'store');
-    Route::get('jobs/{job}/edit', 'edit');
-    Route::patch('jobs/{job}', 'update');
-    Route::delete('jobs/{job}','destroy');
     Route::view("/contact","contact");
-});
+
+
+    Route::get('/jobs',[JobController::class, 'index']);
+    Route::get('jobs/create',[JobController::class,  'create']);
+    Route::get('jobs/{job}', [JobController::class,'show']);
+    Route::post("jobs", [JobController::class, 'store'])->middleware('auth');
+    Route::get('jobs/{job}/edit',[JobController::class, 'edit'])->middleware('auth','can:edit-job,job');
+    Route::patch('jobs/{job}', [JobController::class,'update']);
+    Route::delete('jobs/{job}',[JobController::class,'destroy']);
+ 
+
 
 Route::get('/register',[RegisterUserController::class,'create']);
 Route::post('/register',[RegisterUserController::class,'store']);
 
-Route::get('/login',[SessionController::class,'create']);
+Route::get('/login',[SessionController::class,'create'])->name('login');
 Route::post('/login',[SessionController::class,'store']);
 Route::post('/logout',[SessionController::class,'destroy']);
